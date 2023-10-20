959124c0146b2a5b5fe3c93b6e456fdddb3a0f0b
ArranS24 <141980919+ArranS24@users.noreply.github.com>
2023-10-20 21:11:19 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+var express = require('express');
+var app = express();
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+app.get('/', (req, res) => {
+  res.send(answer);
+});
+
+app.listen(5000);
+console.log('Server is listening on port 5000');
